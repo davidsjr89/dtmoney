2f2d429a239e81d0c86c464ae8e6b1159a8a4130
David Silva Jr <david_sjr@hotmail.com>
2022-03-14 21:32:13 -0300

    Modal and Forms

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
+import { api } from "../../services/apis";
 import { Container } from "./styles";
 
+interface Transaction {
+    id: number;
+    title: string;
+    amount: number;
+    type: string;
+    category: string;
+    createdAt: string;
+}
+
 export function TransactionTable(){
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+    useEffect(() => {
+        api.get('http://localhost:3000/api/transactions')
+        .then(response => setTransactions(response.data.transactions))
+    }, [])
+
+
     return (
         <Container>
             <table>
@@ -13,18 +31,22 @@ export function TransactionTable(){
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td className="title">Desenvolvimento de website</td>
-                        <td className="deposit">R$12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>23/02/2021</td>
+                    {transactions.map(transaction => (
+                        <tr key={transaction.id}>
+                        <td>{transaction.title}</td>
+                        <td className={transaction.type}>
+                            {new Intl.NumberFormat('pt-BR', {
+                                style: 'currency',
+                                currency: 'BRL'
+                            }).format(transaction.amount)}
+                        </td>
+                        <td>{transaction.category}</td>
+                        <td>
+                            {new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}
+                        </td>
                     </tr>
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withdraw">R$1.000</td>
-                        <td>Casa</td>
-                        <td>14/02/2021</td>
-                    </tr>
+                    ))}
+                   
                 </tbody>
             </table>
         </Container>
